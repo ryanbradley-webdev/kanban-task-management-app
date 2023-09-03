b7e5c692f8607070b24a9b90fe51e210bb76df8f
Ryan Bradley <ryanbradley315@gmail.com>
2023-09-03 12:12:14 -0500

    Add header component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,9 @@
 import './App.css'
+import Header from './components/header/Header'
 
 function App() {
   return (
-    <main>
-      
-      <h1>
-        Heading XL
-      </h1>
-      
-      <h2>
-        Heading LG
-      </h2>
-      
-      <h3>
-        Heading MD
-      </h3>
-      
-      <h4>
-        Heading SM
-      </h4>
-
-      <p>
-        Body
-      </p>
-
-    </main>
+    <Header />
   )
 }
 
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import styles from './header.module.css'
+
+export default function Header() {
+    return (
+        <header
+            className={styles.header}
+        >
+
+            Header
+
+        </header>
+    )
+}
