b14bcff23a779c90cdb72c0ec16190c325647a03
Ryan Bradley <ryanbradley315@gmail.com>
2023-09-03 14:23:21 -0500

    Add styling to board selection modal.

--- src/components/boardList/BoardList.tsx
+++ src/components/boardList/BoardList.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import styles from './boardList.module.css'
-import BoardListItem from '../boardListItem/BoardListItem'
 import ThemeToggle from '../themeToggle/ThemeToggle'
+import BoardIcon from '../../assets/BoardIcon'
 
 export default function BoardList() {
     const [boards] = useState(['Platform Launch', 'Marketing Plan', 'Roadmap'])
@@ -19,23 +19,32 @@ export default function BoardList() {
                     ALL BOARDS (3)
                 </h4>
 
-                <ul>
+                <ul
+                    className={styles.items}
+                >
 
-                    {boards.length > 0 && boards.map(board => (
-                        <BoardListItem
-                            key={crypto.randomUUID()}
+                    {boards.length > 0 && boards.map((board, idx) => (
+                        <li
+                            key={idx}
+                            data-selected={idx === 1}
                         >
-                            {board}
-                        </BoardListItem>
+                            <BoardIcon />
+
+                            <span>
+                                {board}
+                            </span>
+                        </li>
                     ))}
 
+                    <li>
+                        <BoardIcon />
+
+                        <span>
+                            &#43; Create New Board
+                        </span>
+                    </li>
+
                 </ul>
-
-                <button>
-
-                    &#43; Create New Board
-
-                </button>
 
                 <ThemeToggle />
 
